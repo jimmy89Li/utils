7639d2114fde920289102aff9715a3735f66a98c
jimmy89Li <liviu.c.simion@gmail.com>
2025-03-15 23:36:59 +0100

    Added memoization to fibonacci sequence in JavaScript

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -3,6 +3,8 @@
  *
  * @param {number} num
  *   The position of the Fibonacci number to return.
+ * @param {object} memo
+ *   An object to store the results of previous calculations.
  *
  * @returns {number}
  *   The Fibonacci number at the given position.
@@ -10,10 +12,15 @@
  * @example
  * fib(10); // 55
  */
-function fib(num) {
+function fib(num, memo = {}) {
+  if (memo[num]) return memo[num];
+
   if (num < 1) return 0;
   if (num === 1) return 1;
-  return fib(num - 1) + fib(num - 2);
+
+  memo[num] = fib(num - 1, memo) + fib(num - 2, memo);
+
+  return memo[num];
 }
 
 console.log(fib(5)); // 5
