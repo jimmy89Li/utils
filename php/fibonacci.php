ae503a39b1b60c8f1eca78b5b6e85c6604a35a0f
jimmy89Li <liviu.c.simion@gmail.com>
2025-03-16 23:14:40 +0100

    Added documentation for Fibonacci sequence implementation in PHP

--- php/fibonacci.php
+++ php/fibonacci.php
@@ -1,5 +1,26 @@
 <?php
 
+/**
+ * Fibonacci sequence implementation.
+ *
+ * The Fibonacci sequence is a series of numbers in which each number is
+ * the sum of the two preceding ones, usually starting with 0 and 1.
+ *
+ * The Fibonacci sequence is defined by the recurrence relation:
+ * F(0) = 0
+ * F(1) = 1
+ * F(n) = F(n-1) + F(n-2)
+ */
+
+/**
+ * Fibonacci sequence implementation.
+ *
+ * @param int $n
+ *   The number of Fibonacci numbers to generate.
+ *
+ * @return void
+ *   The Fibonacci number at position $n.
+ */
 function fibonacci($n): void
 {
     // Using [0,1] will not skip the first number in the sequence,
