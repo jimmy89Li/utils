c61fb3f5ebb4022b5685d51270f6f463c76b4634
jimmy89Li <liviu.c.simion@gmail.com>
2025-03-17 00:38:25 +0100

    Refactor Fibonacci sequence implementation in PHP with recursive, iterative, and dynamic methods; added performance timing for each method.

--- php/fibonacci.php
+++ php/fibonacci.php
@@ -13,25 +13,83 @@
  */
 
 /**
- * Fibonacci sequence implementation.
+ * Recursive implementation of the Fibonacci sequence.
  *
  * @param int $n
- *   The number of Fibonacci numbers to generate.
+ *   The number of the sequence to calculate.
  *
- * @return void
- *   The Fibonacci number at position $n.
+ * @return int
+ *   The value of the sequence at position $n.
  */
-function fibonacci($n): void
+function fibRecursive(int $n): int
 {
-    // Using [0,1] will not skip the first number in the sequence,
-    // while [1,1] will skip the first number in the sequence.
-    $fib = [0, 1];
-    for ($i = 1; $i < $n; $i++) {
-        $fib[] = $fib[$i] + $fib[$i - 1];
+    if ($n <= 1) {
+        return $n;
     }
-
-    echo $fib[$n];
+    return fibRecursive($n - 1) + fibRecursive($n - 2);
 }
 
-// Example: fibonacci for 10 is 55.
-fibonacci(10);
+/**
+ * Iterative implementation of the Fibonacci sequence.
+ *
+ * @param int $n
+ *   The number of the sequence to calculate.
+ *
+ * @return int
+ *   The value of the sequence at position $n.
+ */
+function fibIterative($n): int
+{
+    if ($n <= 1) {
+        return $n;
+    }
+
+    $a = 0;
+    $b = 1;
+
+    for ($i = 0; $i < $n; $i++) {
+        $sum = $a + $b;
+        $a = $b;
+        $b = $sum;
+    }
+    return $a;
+}
+
+/**
+ * Dynamic implementation of the Fibonacci sequence.
+ *
+ * @param int $n
+ *   The number of the sequence to calculate.
+ *
+ * @return array
+ *   The value of the sequence at position $n.
+ */
+function fibDynamic(int $n): int
+{
+    $serie = [0, 1];
+    for ($i = 0; $i < $n; $i++) {
+        $serie[] = $serie[$i] + $serie[$i + 1];
+    }
+    return $serie[$n];
+}
+
+// Aproximately 112ms to run.
+$recursive_start_time = microtime(true);
+echo (fibRecursive(25));
+$recursive_end_time = microtime(true);
+$recursive_execution_time = ($recursive_end_time - $recursive_start_time) * 1000;
+echo "\nrecursive time: $recursive_execution_time\n";
+
+// Aproximately 0.11ms to run.
+$iterative_start_time = microtime(true);
+echo (fibIterative(25));
+$iterative_end_time = microtime(true);
+$iterative_execution_time = ($iterative_end_time - $iterative_start_time) * 1000;
+echo "\niterative time: $iterative_execution_time\n";
+
+// Aproximately 0.11ms to run.
+$dynamic_start_time = microtime(true);
+echo (fibDynamic(25));
+$dynamic_end_time = microtime(true);
+$dynamic_execution_time = ($dynamic_end_time - $dynamic_start_time) * 1000;
+echo "\ndynamic time: $dynamic_execution_time\n";
