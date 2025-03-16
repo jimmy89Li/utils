5324a2b68ad7150956f7fd40d56963fefe2f9a69
jimmy89Li <liviu.c.simion@gmail.com>
2025-03-16 14:30:13 +0100

    ES6 conversion for the Fibonacci sequence in JavaScript

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -12,16 +12,14 @@
  * @example
  * fib(10); // 55
  */
-function fib(num, memo = {}) {
+const fib = (num, memo = {}) => {
   if (memo[num]) return memo[num];
 
   if (num < 1) return 0;
   if (num === 1) return 1;
 
-  memo[num] = fib(num - 1, memo) + fib(num - 2, memo);
-
-  return memo[num];
-}
+  return (memo[num] = fib(num - 1, memo) + fib(num - 2, memo));
+};
 
 console.log(fib(5)); // 5
 console.log(fib(10)); // 55
